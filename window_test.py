9b84bae1a4030f354be0f69162d1999b843551f7
Eliel Taskinen <elieltaskinen@gmail.com>
2021-08-09 21:55:49 +0300

    Added window tests, tried buttons

--- window_test.py
+++ window_test.py
@@ -1,17 +1,24 @@
-from tkinter import *
+#!/usr/bin/python
 
-window = Tk()
+import tkinter as tk
+
+window = tk.Tk()
+
 window.title('DND Initiative tracker')
-
-window.title("Python GUI App")
 window.configure(width=500, height=300)
 window.configure(bg='lightgray')
 
+
+# This positions the window to the middle of the screen
 winWidth = window.winfo_reqwidth()
 winwHeight = window.winfo_reqheight()
 posRight = int(window.winfo_screenwidth() / 2 - winWidth / 2)
 posDown = int(window.winfo_screenheight() / 2 - winwHeight / 2)
 window.geometry("+{}+{}".format(posRight, posDown))
+
+## Start your code here
+
+
 
 
 
